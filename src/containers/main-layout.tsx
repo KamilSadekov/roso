224151a38974e4de5aaf85f09f840c8e45d3037b
laansurpurzzzzz <makden99@yandex.com>
2024-02-13 00:32:31 +0300

    feat(app):app configs

---
+++ src/containers/main-layout.tsx
@@ -0,0 +1,42 @@
+import { PropsWithChildren } from 'react';
+import { Head } from 'next/document';
+
+import cc from 'classcat';
+
+export interface Meta {
+  meta: {
+    description: string;
+    title: string;
+  };
+}
+
+export interface MainLayoutProps extends Meta {
+  contentClassName?: string;
+}
+
+export const MainLayout = ({
+  meta,
+
+  children,
+}: PropsWithChildren<MainLayoutProps>) => (
+  <>
+    <Head>
+      <title>{meta.title || 'Reso'}</title>
+      <meta
+        content={meta.description}
+        name='description'
+      />
+      <meta
+        content='index, follow'
+        name='robots'
+      />
+    </Head>
+
+    <div
+      id='main_container'
+      className={cc(['relative h-full w-full flex-1 pb-4 max-md:pt-4 '])}
+    >
+      {children}
+    </div>
+  </>
+);
