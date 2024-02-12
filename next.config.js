cf8a13ae519613eb2181e77c148acac4be319387
laansurpurzzzzz <makden99@yandex.com>
2024-02-13 00:02:42 +0300

    feat(config):fresh configs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-// @ts-check
-const svg = require('@neodx/svg/webpack');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
@@ -11,26 +9,6 @@ const config = {
     formats: ['image/avif', 'image/webp'],
   },
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    // Prevent doubling svg plugin, let's run it only for client build
-    if (!isServer) {
-      config.plugins.push(
-        svg({
-          root: 'assets/icons',
-          output: 'public/imgs/svg-sprites',
-          group: true,
-          fileName: '{name}.{hash:8}.svg',
-          metadata: {
-            path: 'src/shared/types/icon.ts',
-            runtime: {
-              viewBox: true,
-            },
-          },
-        }),
-      );
-    }
-    return config;
-  }
 };
 
 module.exports = withBundleAnalyzer(config);
