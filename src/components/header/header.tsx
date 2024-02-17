ec49868486d90102d61896fef8a4db42cc8f677e
Kamil Sadekov <sadekovk98@mail.ru>
2024-02-17 15:59:50 +0100

    addded logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,10 @@
+import React, { SVGAttributes } from 'react';
+import Image from 'next/image';
+import logoIcon from 'public/imgs/logo_roso.svg';
 import { ROUTES } from '~/shred/constants/routes';
 import { NavItem } from '~/components/nav-item/nav-item';
 import useTranslation from 'next-translate/useTranslation';
+import Link from 'next/link';
 
 export const Header = () => {
   const { t } = useTranslation('common');
@@ -22,17 +26,29 @@ export const Header = () => {
       route: ROUTES.contact,
     },
   ];
-
+  // TODO: need to add lang select
   return (
-    <div className='flex h-[90px] w-full justify-center'>
+    <div className='flex h-[90px] w-full justify-center py-1.5'>
       <div className='mx-auto flex w-full max-w-[1200px] justify-start'>
-        {headerLinks.map((link, idx) => (
-          <NavItem
-            key={idx}
-            title={link.title}
-            route={link.route}
+        <Link
+          className='h-[75px] w-[75px] self-center'
+          href={'/'}
+        >
+          <Image
+            className='h-full w-full'
+            src={logoIcon as unknown as string}
+            alt='roso logo'
           />
-        ))}
+        </Link>
+        <nav className='ml-5 flex justify-start'>
+          {headerLinks.map((link, idx) => (
+            <NavItem
+              key={idx}
+              title={link.title}
+              route={link.route}
+            />
+          ))}
+        </nav>
       </div>
     </div>
   );
