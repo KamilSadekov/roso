1a65c9ee1be07d5b64c499f64916e106a4bc4f1d
Kamil Sadekov <sadekovk98@mail.ru>
2024-03-19 23:39:09 +0100

    config 1234

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer');
 const nextTranslate = require('next-translate-plugin');
 
-/** @type {import('next').NextConfig} */
-//
-// const isGithubActions = process.env.GITHUB_ACTIONS || false;
-//
-// let assetPrefix = '';
-// let basePath = '/';
-//
-// if (isGithubActions) {
-//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-//
-//   assetPrefix = `/${repo}/`;
-//   basePath = `/${repo}`;
-// }
 const config = {
   images: {
     formats: ['image/avif', 'image/webp'],
@@ -25,4 +12,8 @@ const config = {
   reactStrictMode: true,
 };
 
-module.exports = nextTranslate(withBundleAnalyzer(config));
+module.exports = nextTranslate({
+  webpack: (config) => {
+    return withBundleAnalyzer(config);
+  },
+});
