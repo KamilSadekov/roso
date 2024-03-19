f31f3744611050382889997acb6c65c17d863bea
Kamil Sadekov <sadekovk98@mail.ru>
2024-03-19 19:14:21 +0100

    config 12

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
+const withBundleAnalyzer = require('@next/bundle-analyzer');
 const nextTranslate = require('next-translate-plugin');
 
 /** @type {import('next').NextConfig} */
